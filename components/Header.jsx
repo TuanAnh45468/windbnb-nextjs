2dd98245fc22bf15f99300434482c89d7cb0831b
tuana <tuananh45468@gmail.com>
2023-05-27 21:24:53 +0700

    desktop version

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,14 +3,15 @@ import { Search } from "./Search";
 
 export const Header = (props) => {
   return (
-    <div className={"grid mobile:gap-y-[39px]"}>
+    <div className={"grid mobile:gap-y-[39px] desktop:grid-cols-2"}>
       <Image
+          className={"desktop:self-center desktop:ml-[64px]"}
         src={"/images/logo.svg"}
         alt={"logo"}
         width={96}
         height={19}
       ></Image>
-      <Search {...props} className={"justify-self-center"}></Search>
+      <Search {...props} className={"justify-self-center desktop:justify-self-end"}></Search>
     </div>
   );
 };
